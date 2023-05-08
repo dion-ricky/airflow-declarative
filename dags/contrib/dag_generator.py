8bba06a693d4e430cb128ee81f86e9b8aa09c82b
dion-ricky <dionrickysptr@gmail.com>
2023-05-08 20:22:30 +0700

    Proof of concept of dynamic dag generator

--- dags/contrib/dag_generator.py
+++ dags/contrib/dag_generator.py
@@ -1,14 +1,49 @@
 from airflow.models import DAG
+from airflow.models.baseoperator import BaseOperator
 from airflow.operators.bash import BashOperator
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
+
 class StandardETL:
+
+    def __init__(self, config, default_args) -> None:
+        self.config = config
+        self.default_args = default_args
+
+    def extract_output_path(self, table_name):
+        return f"{table_name}_$dag_interval_start$.csv"
+
+    def add_start_finish(self):
+        pass
+
+    def get_extract_task(self, table_name) -> BaseOperator:
+        return BashOperator(
+            task_id=f"extract_{table_name}",
+            bash_command=f"echo {table_name}",
+            **self.default_args
+        )
+
+    def get_transfer_task(self, table_name) -> BaseOperator:
+        return BashOperator(
+            task_id=f"transfer_{table_name}",
+            bash_command=f"echo {table_name}",
+            **self.default_args
+        )
+
+    def get_load_task(self, table_name) -> BaseOperator:
+        return BashOperator(
+            task_id=f"load_{table_name}",
+            bash_command=f"echo {table_name}",
+            **self.default_args
+        )
 
     @staticmethod
     def generate(config, dag: DAG):
-        dag.add_task(
-            BashOperator(
-                task_id='hello_world',
-                bash_command='echo hello world',
-            )
-        )
+        generator = StandardETL(config, dag.default_args)
+        for tbl in config.keys():
+            extract = generator.get_extract_task(tbl)
+            transfer = generator.get_transfer_task(tbl)
+            load = generator.get_load_task(tbl)
+            dag.add_task(extract)
+            extract.set_downstream(transfer)
+            transfer.set_downstream(load)
--- dags/declarative_dag.py
+++ dags/declarative_dag.py
@@ -4,15 +4,23 @@ from datetime import datetime
 from airflow.models import DAG
 from airflow.settings import DAGS_FOLDER
 
+import yaml
+from yaml.loader import SafeLoader
+
 from contrib.dag_generator import StandardETL
 
 
 dag = DAG(
     dag_id='declarative_dag',
     schedule_interval='@once',
-    start_date=datetime(2023, 5, 6)
+    start_date=datetime(2023, 5, 7),
+    default_args={
+        'owner': 'foo',
+        'retries': 3
+    }
 )
 
-config = open(os.path.join(DAGS_FOLDER, 'config', 'production.yaml'), 'r').read()
+config = yaml.load(open(os.path.join(DAGS_FOLDER, 'config', 'production.yaml'), 'r'),
+                   Loader=SafeLoader)
 
 StandardETL.generate(config, dag)
